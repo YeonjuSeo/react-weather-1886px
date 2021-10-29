f9b586fa41c23b011413fb1c9f121f9e63e03356
YeonjuSeo <tjduswn1219@gmail.com>
2021-10-30 01:36:15 +0900

    🎉  Begin project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled, { createGlobalStyle } from "styled-components";
+import Weather from "./weather/Weather";
 
+const GlobalStyle = createGlobalStyle`
+  *{  
+    box-sizing: border-box;
+  }
+  html {
+    font-size : 10px;
+  }
+  body {
+    width: 100%;
+    margin: 0;
+  }
+`;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className="App">
+      <GlobalStyle />
+      <Weather />
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/weather/Weather.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Weather() {
+  return <div>벗들 화이팅!!</div>;
+}
+
+export default Weather;

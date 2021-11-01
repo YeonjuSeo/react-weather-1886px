ceaf1d4ed9eacec485b3abde49258a16e5f883eb
yyeonkim <seyeon.dev@gmail.com>
2021-11-01 22:25:49 +0900

    Add state for weather object
    가져온 날씨 정보 나타내기

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -1,36 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 
 const API_KEY = "5ff5f673b636e8a4a040a58efff3e499";
 
-const getWeather = async (latitude, longitude) => {
-  const {
-    data: {
-      main: { temp }, // 기온
-      name, // 지역명
-      weather, // weather[0].main -- 날씨
-    },
-  } = await axios.get(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-  );
-};
+export default function Weather() {
+  const [weatherObj, setWetherObj] = useState({});
 
-const success = (position) => {
-  const { latitude, longitude } = position.coords;
-  getWeather(latitude, longitude);
-};
+  const getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp }, // 기온
+        name, // 지역명
+        weather, // weather[0].main -- 날씨
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    const time = new Date().getHours();
+    setWetherObj({ temp, name, weather: weather[0].main, time });
+  };
 
-const error = (err) => {
-  console.log("위치를 찾을 수 없습니다.");
-  // 서울 날씨 알려주기
-};
+  const success = (position) => {
+    const { latitude, longitude } = position.coords;
+    getWeather(latitude, longitude);
+  };
 
-function Weather() {
+  const error = (err) => {
+    console.log("위치를 찾을 수 없습니다.");
+    // 서울 날씨 알려주기
+  };
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success, error);
   });
-  return <div>벗들 화이팅!!</div>;
-}
 
-export default Weather;
+  const { name, temp } = weatherObj;
+
+  return <div>{name + ", " + Math.round(temp) + " °C"}</div>;
+}

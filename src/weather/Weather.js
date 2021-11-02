5c7b91132b77da10591b8926db0146d95a921f16
yyeonkim <seyeon.dev@gmail.com>
2021-11-02 23:31:32 +0900

    Pass weatherObj to return
    날짜, 장소, 날씨, 아이콘, 기온

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 import axios from "axios";
 
+import styled from "styled-components";
+
 const API_KEY = "5ff5f673b636e8a4a040a58efff3e499";
 
 export default function Weather() {
@@ -41,7 +43,43 @@ export default function Weather() {
     navigator.geolocation.getCurrentPosition(success, error);
   });
 
-  const { name, temp } = weatherObj;
+  const { name, temp, icon, weather } = weatherObj;
+  const date = new Date().toDateString();
+  const dateLength = date.length;
 
-  return <div>{name + ", " + Math.round(temp) + " °C"}</div>;
+  return (
+    <Wrapper>
+      <Day>{date.slice(0, dateLength - 5)}</Day>
+      <Place>{name}</Place>
+      <Description>{weather}</Description>
+      <Icon src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+      <Temperature>{Math.round(temp) + " °C"}</Temperature>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.6rem;
+`;
+
+const Day = styled.small``;
+
+const Place = styled.h3`
+  margin: 0;
+  margin: 0.5rem;
+`;
+
+const Description = styled.span`
+  font-size: 2rem;
+`;
+
+const Icon = styled.img`
+  width: 20rem;
+`;
+
+const Temperature = styled.h1`
+  margin: 0;
+  font-size: 4rem;
+`;

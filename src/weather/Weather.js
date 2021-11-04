64f69469de74805394e41ca282074f7d1cf8cf3d
songing01 <79829662+songing01@users.noreply.github.com>
2021-11-05 02:43:22 +0900

    get api data

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -1,7 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import { useState } from "react";
+import axios from "axios";
 
 function Weather() {
-  return <div>벗들 화이팅!!</div>;
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+  const [icon, setIcon] = useState("");
+  const [temp, setTemp] = useState(0);
+
+  const apiKey = "479bfb3e6f988e03fc0081efb206f596";
+
+  const apiCall = axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${apiKey}`
+    )
+    .then((res) => {
+      setCity(res.data.name);
+      setWeather(res.data.weather[0].main);
+      setIcon(res.data.weather[0].icon);
+      setTemp(res.data.main.feels_like);
+    });
+
+  return (
+    <div>
+      <Icon>{icon}</Icon>
+      <Description>Weather : {weather}</Description>
+      <City>City : {city} </City>
+      <Temp>Temp:{(temp - 273.15).toFixed(2)}</Temp>
+    </div>
+  );
 }
 
 export default Weather;
+
+const Description = styled.div``;
+const City = styled.div``;
+const Icon = styled.div``;
+const Temp = styled.div``;

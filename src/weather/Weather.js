7f74bdea70fd4f465e0a0881f4fc1f2e9c15246f
spig0126 <spig0126@gmail.com>
2021-11-05 14:34:01 +0900

    feat: Create date, weather, iconLink with useState

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 function Weather() {
-  return <div>벗들 화이팅!!</div>;
+    const [date, setDate] = useState(null);
+    const [weather, setWeather] = useState({
+        city: '',
+        date: {},
+        weatherType: '',
+        temperature: 0,
+        iconID: '',
+    });
+    const [iconLink, setIconLink] = useState('');
+
+
+    return <h>파이팅!!</h>;
 }
 
 export default Weather;
+
+const StyledWeatherComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  .date
+`

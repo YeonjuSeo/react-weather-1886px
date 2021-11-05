5e2a9e2d83d8f7a0a6bce387edfc5cb77aa93ecd
spig0126 <spig0126@gmail.com>
2021-11-05 14:36:58 +0900

    feat: Update useState values with data from axios.get

--- src/weather/Weather.js
+++ src/weather/Weather.js
@@ -12,6 +12,29 @@ function Weather() {
     });
     const [iconLink, setIconLink] = useState('');
 
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=4571990276b5d671627c9d2ad6de0012&units=metric')
+        .then(function (response) {
+            const data = response.data;
+            const dt = new Date(data.dt * 1000);
+
+            setDate({
+                day: ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'],
+                month: dt.getMonth() + 1,
+                date: dt.getDate(),
+                dayNum: dt.getDay(),
+            });
+            setWeather({
+                city: data.name,
+                weatherType: data.weather[0].main,
+                temperature: data.main.temp,
+                iconID: data.weather[0].icon,
+            });
+            setIconLink(`http://openweathermap.org/img/wn/${weather.iconID}@2x.png`);
+
+            console.log(Date);
+            console.log(weather);
+            console.log(iconLink);
+        });
 
     return <h>파이팅!!</h>;
 }
